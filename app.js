490b9035988a5635f3ddc2f299aa7885256e2524
Taylor Chen <s68175@gmail.com>
2020-07-15 22:39:07 +0800

    [Project] Add movielist with Express
    [Description]
    1. Create movielist with Express
    2. Add template engine - Handlebars

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// routes setting
+app.get('/', (req, res) => {
+  // res.send('This is my movie list built with Express')
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
