8366e08cddb87c56b041619e5b7f0ed0aed4551f
Taylor Chen <s68175@gmail.com>
2020-07-16 00:04:34 +0800

    Add bootstrap settings

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 // routes setting
 app.get('/', (req, res) => {
   // res.send('This is my movie list built with Express')
