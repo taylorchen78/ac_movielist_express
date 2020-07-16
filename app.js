5ba17b7b73ea4725c95869c109615e412f55f397
Taylor Chen <s68175@gmail.com>
2020-07-16 21:54:03 +0800

    Change movielist by json file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -11,8 +13,50 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  // res.send('This is my movie list built with Express')
-  res.render('index')
+  // const movieList = [
+  //   {
+  //     id: 1,
+  //     title: 'Jurassic World: Fallen Kingdom',
+  //     image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'THIS IS MOVIE TITLE',
+  //     image: 'https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg'
+  //   }, {
+  //     id: 3,
+  //     title: "Thor: Ragnarok",
+  //     image: "https://movie-list.alphacamp.io/posters/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg"
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Avengers: Infinity War",
+  //     image: "https://movie-list.alphacamp.io/posters/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Mission: Impossible - Fallout",
+  //     image: "https://movie-list.alphacamp.io/posters/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg"
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Incredibles 2",
+  //     image: "https://movie-list.alphacamp.io/posters/x1txcDXkcM65gl7w20PwYSxAYah.jpg"
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "Fifty Shades Freed",
+  //     image: "https://movie-list.alphacamp.io/posters/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg"
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "The First Purge",
+  //     image: "https://movie-list.alphacamp.io/posters/2slvblTroiT1lY9bYLK7Amigo1k.jpg"
+  //   },
+  // ]
+
+  // past the movie data into 'index' partial template
+  res.render('index', { movies: movieList.results })
 })
 
 // start and listen on the Express server
